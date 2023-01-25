697fe053ec468591d43d49441254de352ef64e6d
Ilya Yalymov <chezeee@gmail.com>
2023-01-25 21:02:31 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} text
+ */
+const getUnit = text => {
+	// your code    
+}
+
+
+// Sample usage - do not modify
+getUnit("It was 90°F yesterday"); // Fahrenheit
+getUnit("Why is it 20°C"); // Celsius
+getUnit("It is expected to be cold."); // N/A
