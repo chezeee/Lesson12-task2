784c5adfcc547791ce16ebb9d3d5373057fc5549
Ilya Yalymov <chezeee@gmail.com>
2023-01-25 21:20:00 +0300

    Problem solution added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 /**
  * @param {string} text
  */
-const getUnit = text => {
-	// your code    
-}
-
+const getUnit = (text) => {
+  if (text.includes("°F")) return "Fahrenheit";
+  else if (text.includes("°C")) return "Celsius";
+  return "N/A";
+};
 
 // Sample usage - do not modify
 getUnit("It was 90°F yesterday"); // Fahrenheit
